47f217d8fe5134638924d5f425de73080f907d7a
Barry Hanna <barryahanna@gmail.com>
2023-06-21 08:19:19 +0100

    Exercise 1.6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	const handleClick = (e) => {
+		const { innerText: buttonVal } = e.target;
+		console.log(buttonVal);
+		switch (buttonVal) {
+			case 'good':
+				const newGood = good + 1;
+				setGood(newGood);
+				break;
+			case 'bad':
+				const newBad = bad + 1;
+				setBad(newBad);
+				break;
+			case 'neutral':
+				const newNeutral = neutral + 1;
+				setNeutral(newNeutral);
+				break;
+			default:
+				return;
+		}
+	};
+
+	return (
+		<div>
+			<div>
+				<p>give feedback</p>
+				<button onClick={handleClick}>good</button>
+				<button onClick={handleClick}>neutral</button>
+				<button onClick={handleClick}>bad</button>
+			</div>
+			<div>
+				<p>statistics</p>
+				<p>good {good}</p>
+				<p>neutral {neutral}</p>
+				<p>bad {bad}</p>
+			</div>
+		</div>
+	);
 }
 
 export default App;
